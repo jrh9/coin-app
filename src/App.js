88e3f2f9042beb610bd3acfed387bbbfb72ed900
Jon <jon.hernandez93@gmail.com>
2018-05-10 20:17:31 -0700

    Add basic routing and notfound component

--- src/App.js
+++ src/App.js
@@ -1,16 +1,23 @@
 import React, { Component } from 'react';
 import './App.css';
+import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
+import NotFound from './components/notfound/NotFound';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <Header />
+      <BrowserRouter>
+        <div>
+          <Header />
 
-        <List />
-      </div>
+          <Switch>
+            <Route path="/" component={List} exact />
+            <Route component={NotFound} />
+          </Switch>
+        </div>
+      </BrowserRouter>
     );
   }
 }
