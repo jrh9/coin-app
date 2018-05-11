1b9c3cfc8ec4cfcf03eff553c134a7efbce27357
Jon <jon.hernandez93@gmail.com>
2018-05-10 20:23:25 -0700

    Add basic details page for currency

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import { BrowserRouter, Route, Switch } from 'react-router-dom';
 import Header from './components/common/Header';
 import List from './components/list/List';
 import NotFound from './components/notfound/NotFound';
-
+import Detail from './components/detail/Detail';
 class App extends Component {
   render() {
     return (
@@ -14,6 +14,7 @@ class App extends Component {
 
           <Switch>
             <Route path="/" component={List} exact />
+            <Route path="/currency/:id" component={Detail} exact/>
             <Route component={NotFound} />
           </Switch>
         </div>
---
+++ src/components/detail/Detail.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './Detail.css';
+
+class Detail extends React.Component {
+    render() {
+        return (
+          <div>Details</div> 
+        );
+    }
+}
+
+export default Detail;
